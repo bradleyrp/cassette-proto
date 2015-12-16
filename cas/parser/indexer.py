1df261af961f7ec55e847953564fc16054b3e5ca
Ryan Bradley <bradleyrp@gmail.com>
2015-12-16 13:33:28 -0500

    removed redundant links from indexer

--- cas/parser/indexer.py
+++ cas/parser/indexer.py
@@ -7,11 +7,13 @@ ltype = ['ol','ul'][-1]
 html = ["""<link rel="stylesheet" href="./cas/sources/main.css" type="text/css"/>"""]
 html += ["<title>drafts</title><body>"]
 html += ["""<div id="wrapper"><div id="main_content"><h1>drafts</h1>"""]
-html += ["""<a style="color: black;" href="../index.html">back to main page</a>"""]
+if 0: html += ["""<a style="color: black;" href="../index.html">back to main page</a>"""]
+if 0: html += ["""<a href="javascript:history.go(-1)" style="color:black;">return</a>"""]
 html += ["""<div class="article_text">"""]
 
 html += ["<h3>HTML</h3><%s>"%ltype]
 fns = glob.glob('*.html')
+if 'index.html' in fns: fns.remove('index.html')
 for fn in fns: html += ["<li><a href=%s>%s</a></li>"%(fn,re.findall('^(.+)\.html',fn)[0])]
 html += ["</%s>"%ltype]
 
